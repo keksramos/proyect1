b8a2d8ddc2da95004440fb772b835ba3afc5edd8
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-29 15:34:24 -0600

    visualization of posts

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,48 +1,42 @@
 import React from 'react';
 
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 import Post from '../Post/Post'
 
 function App() {
   const posts = [
     {
-      id:"01",
       title:"Was Rust Worth It? From JavaScript to Rust, three years in.", 
       author:"Jarrod Overson", 
       creationDate:"10-25-2023", 
       description:"A few years ago, I dropped everything to focus 100% on WebAssembly. At the time, Rust had the best support for compiling into WebAssembly, and the most full-featured WebAssembly runtimes were Rust-based. Rust was the best option on the menu. I jumped in, eager to see what all the hype was about.",
     },
     {
-      id:"02",
       title:"Kubernetes And Kernel Panics", 
       author:"Kyle Anderson", 
       creationDate:"10-27-2023", 
       description:"With a recent effort to reduce customer (engineers, not end users) pain on our container platform Titus, I started investigating “orphaned” pods. There are pods that never got to finish and had to be garbage collected with no real satisfactory final status. Our Service job (think ReplicatSet) owners don’t care too much, but our Batch users care a lot. Without a real return code, how can they know if it is safe to retry or not?",
     },
     {
-      id:"03",
       title:"A personal, non-partisan perspective on the Israel-Hamas war. To understand this war, we must understand the thousand-year history that led us here", 
       author:"Isaac Saul", 
       creationDate:"10-12-2023", 
       description:"People ask me all the time if I am “pro-Israel” because I am a Jew who has lived in Israel, and my answer is that being “pro-Israel” or being “pro-Palestine” or being a “Zionist” does not properly capture the nuance of thought most people do or should have about this issue. It certainly doesn’t capture mine.",
     },
     {
-      id:"04",
       title:"TimeGPT: The First Foundation Model for Time Series Forecasting", 
       author:"Marco Peixeiro", 
       creationDate:"10-24-2023", 
       description:"The field of time series forecasting is going through a very exciting period. In only the last three years, we have seen many important contributions, like N-BEATS, N-HiTS, PatchTST and TimesNet.",
     },
     {
-      id:"05",
       title:"My Husband Could Never Find Out. He would have been furious if he’d known where I had been", 
       author:"Claire Franky", 
       creationDate:"10-26-2023", 
       description:"I’d set my alarm an hour early, showered, styled my hair, and applied makeup while trying to stay calm. I then woke my eight-month-old daughter and fed and clothed her.",
     },
     {
-      id:"06",
       title:"Sheet, dialog, or snackbar — what should a designer go for? Identifying established patterns in the market.", 
       author:"Ksenia Toloknova", 
       creationDate:"10-25-2023", 
@@ -53,7 +47,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Post/>
+        {
+          posts.map((post, index) => {
+            return <Post key={index} title={post.title} author={post.author} creationDate={post.creationDate} description={post.description}/>
+          })
+        }
       </header>
     </div>
   );
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ('./post.scss')
 
-function Post(){
+function Post(props){
+    const {title, author, creationDate, description} = props;
 
-    return <div className="postContainer">This is a post!</div>;
+    return <div className="postContainer">
+        <p>{title}</p>
+        <p>{author}</p>
+        <p>{creationDate}</p>
+        <p>{description}</p>
+    </div>;
 }
 
 export default Post;
