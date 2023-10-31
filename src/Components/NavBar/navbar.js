8bf1e66dfe818baae05a85420a5b619333904c4d
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-10-30 22:09:24 -0600

    navbar scss

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./navbar.scss"
 
 
 export const Navbar = () => {
+    const navref = useRef()
+
+    const showNavBar = () => {
+        navref.current.classList.toggle("responsive_nav")
+    }
+
     return (
         <header>
             <h3>Logo</h3>
-            <nav>
+            <nav ref={navref}>
                 <a href="/#">Our story</a>
                 <a href="/#">Membership</a>
                 <a href="/#">Write</a>
                 <a href="/#">Sign In</a>
-                <button>Get started</button>
+                <button className="navButton">Get started</button>
             </nav>
         </header>
     )
