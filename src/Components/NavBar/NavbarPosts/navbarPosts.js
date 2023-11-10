ec8494313d89f574eda474b3ab4574bf39864b29
ceciliaramoscavazos <aceciliaramos@gmail.com>
2023-11-09 23:48:34 -0600

    navbar completed

--- src/Components/NavBar/NavbarPosts/navbarPosts.js
+++ src/Components/NavBar/NavbarPosts/navbarPosts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaPencilAlt, FaRegBell, FaUser} from "react-icons/fa"
 
 import "../NavbarPosts/navbarPosts.scss";
@@ -6,15 +6,12 @@ import "../NavbarPosts/navbarPosts.scss";
 import smallLogo from "../../../../public/logoWrite.png";
 
 export const NavbarPosts = () => {
-  // const [navbar, setNavbar] = useState(false);
 
   return (
     <nav className="navbarPosts">
-      <div>
+      <div className="leftSectionNavbar">
       <img className="navbar-logo" src={smallLogo} width={60} ></img>
-      {/* <form>
-      <input>Search</input>
-      </form> */}
+      <input className="searchInNavBar" type="text" placeholder="Search"></input>
       </div>
       <div>
         <button className="navbarPostButton" href={"/write"}> <FaPencilAlt size={15}/> Write</button>
